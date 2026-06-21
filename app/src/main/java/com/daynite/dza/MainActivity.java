1c54558dfa2c03d59d8a63bcfdc2abdf9d9fba91
dazaanmu-design <dazaanmu@gmail.com>
2026-06-22 03:08:00 +0700

    Update HOME_URL to new Netlify address

--- app/src/main/java/com/daynite/dza/MainActivity.java
+++ app/src/main/java/com/daynite/dza/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
     private LinearLayout errorView;
     private ValueCallback<Uri[]> filePathCallback;
     private static final int FILE_CHOOSER_REQUEST_CODE = 1001;
-    private static final String HOME_URL = "https://dza.netlify.app/";
+    private static final String HOME_URL = "https://dza-v15.netlify.app/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
